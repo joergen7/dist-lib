d47d92f3ab5549d70493e9c5f5b65147300dd05e
Jörgen Brandt <joergen@cuneiform-lang.org>
2025-12-13 12:08:55 +0100

    remove dependency on draw

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "dist-lib")
-(define deps '("base" "draw-lib"))
+(define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/dist-lib.scrbl" ())))
 (define pkg-desc "A library for distance-based algorithms.")
